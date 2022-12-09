260d8ebb9a7c84cec56f83e8f0947b3ada786ca0
HouceinBenjeddou <hou52cine@gmail.com>
2022-12-09 19:03:05 +0100

    init

--- sunny/src/components/Hero.jsx
+++ sunny/src/components/Hero.jsx
@@ -2,17 +2,14 @@ import React from 'react'
 import { hdmicro, line, price, delivery } from '../assets'
 const Hero = () => {
   return (
-  <section id='home' className='container mx-auto '>
+  <section id='home' className='container '>
     <div className=' w-full py-10  flex justify-between items-center relative '>
       <div className="container flex justify-center max-w-lg">
-      <img src={line} alt='line' className='z-10 w-full object-cover bg-no-repeat h-96 flex center
-        flex-1 '/>
-
-        <img src={hdmicro} alt='hdmicro' className='z-40 absolute ml-6 -right-[100px] px-3 py-1 w-[510px] h-[660px]
-        '/>
-        <img src={price} alt='price' className='z-30 initial
-         right-[150px] bottom-[190px] '/>
+         <img src={line} alt='line' className='z-10 object-contain bg-no-repeat '/>
+         <img src={hdmicro} alt='hdmicro' className='z-40 absolute ml-6 -right-[100px] px-3 py-1 w-[510px] h-[660px]'/>
+         <img src={price} alt='price' className='z-30 initial right-[150px] bottom-[190px] '/>
       </div>
+       
        {/*
         <div className='relative left-8 flex flex-col '>
         <span>
@@ -24,6 +21,7 @@ const Hero = () => {
           <button>Add to Cart</button>
           <span>How it Works</span>
       </div>  */}
+
     </div>
   </section>
   )
